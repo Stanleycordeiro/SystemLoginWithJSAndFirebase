b48a0569e30e317152eaad93119880671d92becb
Stanleycordeiro <stanley.cordeiro@outlook.com.br>
2023-06-30 23:01:04 -0300

    tratamento mensagens de erro login e email de recuperação de senha incluso

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,8 @@
 // elementos
 const btnLogin = document.getElementById("btnLogin");
-const btnRecuperar = document.getElementById("btnRecuperar");
+const btnRecuperar = document.getElementById("btnRecupere");
 const inputEmail = document.getElementById("inputEmail");
 const inputPassword = document.getElementById("inputPassword");
-
-
-
 
 // liberar botão de login após verificar e-mail válido
 function onChangeEmail() {
@@ -20,7 +17,7 @@ function onChangePassword() {
 }
 
 //desabilitar tecla enter
-document.addEventListener('keypress', function(event) {
+document.addEventListener("keypress", function (event) {
   var key = event.which || event.keyCode;
   if (key === 13) {
     event.preventDefault();
@@ -38,6 +35,25 @@ function toggleBtnLogin() {
   const emailValid = isEmailValid();
   const passwordValid = isPasswordValid();
   btnLogin.disabled = !emailValid || !passwordValid;
+}
+
+//função recuperar senha
+function recoveryPassword() {
+  let email = inputEmail.value;
+
+  showLoading();
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email)
+    .then(() => {
+      hideLoading();
+      alert("Email de alteração de senha enviado com sucesso");
+      btnRecuperar.style.display = "none";
+    })
+    .catch((error) => {
+      hideLoading();
+      alert(getErrorMessage(error));
+    });
 }
 
 //redireciona para a pagina home
@@ -60,10 +76,21 @@ function login() {
 }
 
 function getErrorMessage(error) {
-  if (error.code == "auth/user-not-found") {
-    return "Usuario não cadastrado";
+  // if (error.code == "auth/user-not-found") {
+  //   return "Usuario não cadastrado";
+  // }
+  // return error.message;
+  switch (error.code) {
+    case "auth/invalid-email":
+      return "E-mail inválido. Por favor, insira um e-mail válido.";
+    case "auth/user-not-found":
+      return "Usuário não encontrado. Verifique o e-mail informado.";
+    case "auth/wrong-password":
+      btnRecuperar.style.display = "block";
+      return "Senha incorreta. Verifique a senha ou clique em recuperar senha";
+    default:
+      return error.message;
   }
-  return error.message;
 }
 
 function register() {
